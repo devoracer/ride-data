ff3676cfea05dbac8b61bc285e13e6b56772e0b4
Chris Peterson <chriswilliampeterson@gmail.com>
2016-04-20 18:51:43 -0600

    puts out max speed for all 30 of history

--- Stravatest.rb
+++ Stravatest.rb
@@ -5,24 +5,25 @@ TOKEN = ENV['STRAVA_TOKEN']
 client = Strava::Api::V3::Client.new(:access_token => TOKEN)
 activities = client.list_athlete_activities
 
-max_speedms = activities[1]["max_speed"]
+# max_speedms = activities[3]["max_speed"]
 
-max_speedmph = (max_speedms * 2.23694).round(2)
-puts "Your max speed was #{max_speedmph}mph!"
+# max_speedmph = (max_speedms * 2.23694).round(2)
+# puts "Your max speed was #{max_speedmph}mph!"
 
-elev_gain = activities[1]["total_elevation_gain"]
-elev_gainft = (elev_gain * 3.28084).round(2)
-puts "You gained #{elev_gainft}ft of elevation"
+# elev_gain = activities[3]["total_elevation_gain"]
+# elev_gainft = (elev_gain * 3.28084).round(2)
+# puts "You gained #{elev_gainft}ft of elevation"
 
 # max_speedmsall = activities["max_speed"]
 
 # max_speedmphall = (max_speedmsall * 2.2694).round(2)
 # puts "your max speeds were #{max_speedmphall}mph"
 
-# # pp activities
-# puts activities.length
+# pp activities
+puts activities.length
 activities.each do |activity|
   name = activity["name"]
   max_speedms = activity["max_speed"]
-  puts "On #{name} your max speed was #{max_speedms}."
+  max_speedmph = (max_speedms * 2.23694).round(2)
+  puts "On #{name} your max speed was #{max_speedmph}."
 end
