6490d8cfb7a62962f1f0cf7d1b05dabca1973629
Chris Peterson <chriswilliampeterson@gmail.com>
2016-04-23 15:42:39 -0600

    ALL the data
    printed out all ride names
    figured out how many activities per page and how many pages total

--- Stravatest.rb
+++ Stravatest.rb
@@ -3,7 +3,30 @@ require 'strava/api/v3'
 #replace with your token, found at https://www.strava.com/settings/api
 TOKEN = ENV['STRAVA_TOKEN']
 client = Strava::Api::V3::Client.new(:access_token => TOKEN)
-activities = client.list_athlete_activities
+# activities = client.list_athlete_activities(per_page: 30, page: 1)
+
+current_page = 1
+
+loop do
+	# actions to take on the current page
+	activities = client.list_athlete_activities(page: current_page)
+	
+	activities.each do |activity|
+	  	name = activity["name"]
+	  	max_speedms = activity["max_speed"]
+	  	max_speedmph = (max_speedms * 2.23694).round(2)
+	  	puts name
+	end
+
+	if  activities.length < 30
+		break
+	end
+
+	current_page += 1
+end 
+
+# Loop through pages, printing off each page. until there's less than
+# 30 activites per page
 
 # max_speedms = activities[3]["max_speed"]
 
@@ -19,11 +42,11 @@ activities = client.list_athlete_activities
 # max_speedmphall = (max_speedmsall * 2.2694).round(2)
 # puts "your max speeds were #{max_speedmphall}mph"
 
-# pp activities
-puts activities.length
-activities.each do |activity|
-  name = activity["name"]
-  max_speedms = activity["max_speed"]
-  max_speedmph = (max_speedms * 2.23694).round(2)
-  puts "On #{name} your max speed was #{max_speedmph}."
-end
+# # pp activities
+# puts activities.length
+# activities.each do |activity|
+#   name = activity["name"]
+#   max_speedms = activity["max_speed"]
+#   max_speedmph = (max_speedms * 2.23694).round(2)
+#   puts "On #{name} your max speed was #{max_speedmph}."
+# end
